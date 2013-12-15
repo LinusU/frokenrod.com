d4130747b86606cc97d4891b4c9c9c6976c48c35
Linus Unnebäck <linus@folkdatorn.se>
2013-12-15 22:31:13 +0100

    second draft

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var stylus = require('stylus');
 var express = require('express');
 
 var gmail = require('./lib/gmail');
+var gallery = require('./lib/gallery');
 var instagram = require('./lib/instagram');
 
 var app = express();
@@ -16,15 +17,13 @@ app.set('view engine', 'jade');
 app.use('/img', express.static(path.join(__dirname, 'assets')));
 app.use(express.urlencoded());
 
-app.get('/', function (req, res, next) {
-  instagram.feed(function (err, feed) {
-    if (err) {
-      next(err);
-    } else {
-      res.render('landing', { feed: feed });
-    }
-  });
-});
+app.get('/', instagram.endpoint('landing', /^(?!(Brick(a|or)|Grytunderlägg))/));
+app.get('/tray', instagram.endpoint('tray', /^Brick(a|or)/));
+app.get('/coaster', instagram.endpoint('coaster', /^Grytunderlägg/));
+
+app.get('/card', gallery.index);
+app.get('/card/:album', gallery.album);
+app.get('/card/:album/:b64.png', gallery.file);
 
 app.get('/contact', function (req, res) {
   res.render('contact-get');
@@ -34,31 +33,6 @@ app.post('/contact', function (req, res) {
   gmail.sendMail(req.body.name, req.body.mail, req.body.body, function (err) {
     res.render('contact-post', { success: (err === null) });
   });
-});
-
-app.get('/gallery', function (req, res) {
-  res.render('gallery-index');
-});
-
-app.get('/gallery/:album', function (req, res) {
-  fs.readdir(path.join(__dirname, 'gallery', req.params.album), function (err, files) {
-    var imgs = files.filter(function (e) {
-      return (path.extname(e) === '.png');
-    }).map(function (e) {
-      var abspath = path.join(__dirname, 'gallery', req.params.album, e);
-      var b64 = new Buffer(e).toString('base64');
-      return {
-        title: path.basename(abspath, '.png'),
-        url: '/gallery/' + req.params.album + '/' + encodeURIComponent(b64) + '.png'
-      };
-    });
-    res.render('gallery-album', { album: req.params.album, imgs: imgs });
-  });
-});
-
-app.get('/gallery/:album/:b64.png', function (req, res) {
-  var fileName = new Buffer(req.params.b64, 'base64').toString();
-  res.sendfile(path.join(__dirname, 'gallery', req.params.album, fileName));
 });
 
 app.get('/style', function (req, res, next) {
